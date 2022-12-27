90663ce4f37d49a8d7a278fe88768d37172db5ae
fatihgenc <gnc.fath@gmail.com>
2022-12-28 01:36:35 +0200

    add: resolver functions to retrieve data using query functions

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,3 +1,31 @@
+import {
+  getUsers,
+  getJobs,
+  getJobById,
+  getUserById,
+  getCompanyById,
+  getCompanyJobsById,
+} from '../utils/queries.js';
+
 export const resolvers = {
-  Query: {},
+  Query: {
+    jobs: () => getJobs(),
+    users: () => getUsers(),
+    job: (_root, { id }) => getJobById(id),
+    user: (_root, { id }) => getUserById(id),
+    company: (_root, { id }) => getCompanyById(id),
+  },
+  User: {
+    company: (user) => {
+      return getCompanyById(user?.companyId);
+    },
+    job: (user) => {
+      return getJobById(user?.jobId);
+    },
+  },
+  Company: {
+    availableJobs: (company) => {
+      return getCompanyJobsById(company.id);
+    },
+  },
 };
