31e60609f1778164ab37f48c2bbe1eaa9ffe1646
fatihgenc <gnc.fath@gmail.com>
2023-01-11 02:20:31 +0200

    add: avatar field to user data and add that field to schema as well

--- server/data/users.js
+++ server/data/users.js
@@ -1,15 +1,19 @@
+import { faker } from '@faker-js/faker';
+
 export const users = [
   {
     id: 'user_1',
     fullname: 'fatih genc',
     jobId: 'job_2',
     companyId: 'company_1',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_2',
     fullname: 'oznur yilmaz',
     jobId: 'job_1',
     companyId: 'company_1',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_3',
@@ -20,35 +24,41 @@ export const users = [
     fullname: 'mehmet genc',
     jobId: 'job_3',
     companyId: 'company_3',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_5',
     fullname: 'habil yadigar',
     jobId: 'job_6',
     companyId: 'company_4',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_6',
     fullname: 'beytullah tiryaki',
     jobId: 'job_3',
     companyId: 'company_5',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_7',
     fullname: 'gokay meric sisik',
     jobId: 'job_5',
     companyId: 'company_4',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_8',
     fullname: 'steve jobs',
     jobId: 'job_7',
     companyId: 'company_2',
+    avatar: faker.image.avatar(),
   },
   {
     id: 'user_9',
     fullname: 'fatih akar',
     jobId: 'job_4',
     companyId: 'company_3',
+    avatar: faker.image.avatar(),
   },
 ];
